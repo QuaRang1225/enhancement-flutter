e3145ac551372bdb8675be78bf3ad3bbe818d92a
QuaRang <dbduddnd1225@gmail.com>
2025-02-05 12:05:34 +0900

    - 배경 그라이데이션 적용

--- lib/Alert.dart
+++ lib/Alert.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Alert extends StatelessWidget {
-  const Alert({super.key});
+  List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext con){
-                return alert(context);
-              }
-          );
-        },
-        child: Text(
-          "touch",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.1,0.2,0.3,0.4,0.5,0.6,0.7]
+        ),
+      ),
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext con){
+                  return alert(context);
+                }
+            );
+          },
+          child: Text(
+            "touch",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black
+            ),
           ),
         ),
       ),
