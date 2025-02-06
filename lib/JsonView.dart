b6890058da30fc75a65b28710a547c793c6af064
QuaRang <dbduddnd1225@gmail.com>
2025-02-06 19:09:16 +0900

    - json 데이터 SharedPreferencs로 저장 후 불러오기 기능 구현

--- lib/JsonView.dart
+++ lib/JsonView.dart
@@ -1,32 +1,68 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class JsonView extends StatelessWidget {
+class JsonView extends StatefulWidget {
   const JsonView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Future loadJson() async {
-      final String response = await rootBundle.loadString("response.json");
-      final data = await json.decode(response);
-      return data;
-    }
+  JsonViewState createState() => JsonViewState();
+}
 
+class JsonViewState extends State<JsonView> {
+  late SharedPreferences pref;
+  String entryTitle = "";
+  String entryVersion = "";
+  final TextEditingController con = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    initPrefs();
+  }
+
+  // SharedPreferences 초기화 및 데이터 로드
+  Future initPrefs() async {
+    pref = await SharedPreferences.getInstance();
+  }
+
+  Future loadJson() async {
+    final String response = await rootBundle.loadString("response.json");
+    final data = await json.decode(response);
+    return data;
+  }
+
+  void saveData(String version, String title) async {
+    await pref.setString("version", version);
+    await pref.setString("title", title);
+  }
+
+  void getData() {
+    setState(() {
+      entryVersion = pref.getString("version") ?? "";
+      entryTitle = pref.getString("title") ?? "";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     Future pokemonInfo = loadJson();
     return Scaffold(
-        appBar: AppBar(
-          title: Text("포켓몬 정보"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(10),
-          child: FutureBuilder(
-              future: pokemonInfo,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                      itemCount:
-                          snapshot.data["data"]["text_entries"]["text"].length,
+      appBar: AppBar(
+        title: Text("포켓몬 정보"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: FutureBuilder(
+          future: pokemonInfo,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data["data"]["text_entries"]["text"].length,
                       itemBuilder: (_, index) {
                         return Row(
                           children: [
@@ -35,8 +71,8 @@ class JsonView extends StatelessWidget {
                               height: 70,
                               alignment: Alignment.center,
                               decoration: BoxDecoration(
-                                color: Colors.grey.withOpacity(0.2), // 배경색
-                                borderRadius: BorderRadius.circular(10), // 모서리 둥글게
+                                color: Colors.grey.withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(10),
                               ),
                               margin: EdgeInsets.all(5),
                               child: Text(
@@ -44,28 +80,88 @@ class JsonView extends StatelessWidget {
                                 style: TextStyle(fontWeight: FontWeight.bold),
                               ),
                             ),
-                            Container(
-                              margin: EdgeInsets.all(5),
-                              child: Text(
-                                "${snapshot.data["data"]["text_entries"]["text"][index]}",
-                                maxLines: 3,
+                            Expanded(
+                              child: Container(
+                                margin: EdgeInsets.all(5),
+                                child: Text(
+                                  "${snapshot.data["data"]["text_entries"]["text"][index]}",
+                                  maxLines: 3,
+                                ),
                               ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                saveData(
+                                  snapshot.data["data"]["text_entries"]["version"][index],
+                                  snapshot.data["data"]["text_entries"]["text"][index],
+                                );
+                              },
+                              child: Text("저장"),
                             ),
                           ],
                         );
-                      });
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text("${snapshot.error}"),
-                  );
-                } else {
-                  return Center(
-                      child: CircularProgressIndicator(
-                    strokeWidth: 5,
-                  ));
-                }
-              }),
-        )
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: getData,
+                    child: Text("불러오기"),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Column(
+                      children: [
+                        Row(children: [
+                          Container(
+                            width:100,
+                            alignment: Alignment.center,
+                            child: Text("저장버전",style: TextStyle(fontWeight: FontWeight.bold),)
+                          ),
+                          Expanded(
+                              child: Center(child: Text("저장 텍스트")),
+                          )
+                        ],
+                        ),
+                        Row(
+                          children: [
+                          Container(
+                              width: 100,
+                              height: 70,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius:BorderRadius.circular(10),
+                                color: Colors.grey.withOpacity(0.2),
+                              ),
+                              child: Text(entryVersion)
+                          ),
+                          Expanded(
+                            child: Container(
+                                padding: EdgeInsets.only(left: 10),
+                                height: 100,
+                                alignment: Alignment.centerLeft,
+                                child: Text(entryTitle)
+                            )
+                          )
+                        ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("${snapshot.error}"),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(strokeWidth: 5),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
